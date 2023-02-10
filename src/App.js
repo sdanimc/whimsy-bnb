a475c4705250f8812a3890a29cf9afd612cfc274
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-02-10 11:48:22 -0800

    changed file structure, imported page files into page component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
+//import components used on all pages
 import Header from "./components/header";
+import Nav from "./components/footer";
+import Footer from "./components/footer";
+//import pages 
 
 
 function App() {
---
+++ src/components/page/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PageContent from "../pagecontent";
+//import about pages
+import AboutGhosts from "../../pages/about/ghosts";
+import AboutHistory from "../../pages/about/history";
+import AboutMaps from "../../pages/about/maps";
+import AboutStaff from "../../pages/about/staff";
+
+//import activities pages
+import Tours from "../../pages/activities/ghosttours";
+import LocalAdventures from "../../pages/activities/localadventures";
+
+//import booking info pages
+import Rooms from "../../pages/bookcontact/roomopts";
+import Calendar from "../../pages/bookcontact/calendar";
+import Contact from "../../pages/bookcontact/contact";
+import Events from "../../pages/bookcontact/eventopts";
+
+//import home page
+import Home from "../../pages/home";
+
+function Page({ currentPage }) {
+
+}
+
+export default Page;

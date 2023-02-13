1e136f07cecb871424acdadca8d53545ae2268df
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-02-13 10:07:59 -0800

    clicking dropdown options now renders that pages content, currently only applies to staff will fill out for all menu options next

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import Dropdown from "./dropdown";
+//import Dropdown from "./dropdown";
 
 function Nav(props) {
     const {
@@ -7,19 +7,30 @@ function Nav(props) {
         setCurrentPage,
         currentPage,
     } = props;
-  
+
     return (
         <nav>
             <ul className="menus">
                 {pages.map((Pagea) => (
-                <li className={`mx-5 ${currentPage.name === Pagea.name && 'navActive'} ${Pagea.name}`} key={Pagea.name}>
-                    <span onClick={() => setCurrentPage(Pagea)}>
-                   {/* <span onClick={function(){setCurrentPage({name: Pagea.name})}}>*/}
-                        {Pagea.name}
+                    <li className={`mx-5 ${currentPage.name === Pagea.name && 'navActive'} ${Pagea.name}`} key={Pagea.name}>
+                        <span onClick={() => setCurrentPage(Pagea)}>
+                            {/* <span onClick={function(){setCurrentPage({name: Pagea.name})}}>*/}
+                            {Pagea.name}
                         </span>
-                        <Dropdown />
-                </li>
-            ))}
+                        {/*<Dropdown />*/}
+                        <div className="dropdowns">
+                            <ul className="dropdown aboutdown">
+                                <li className={`dropOpt`}> <span onClick={function () { setCurrentPage({ name: "Staff" }) }}>Staff</span></li>
+                            </ul>
+                            <ul className="dropdown activdown">
+                                <li>drop2</li>
+                            </ul>
+                            <ul className="dropdown bookdown">
+                                <li>drop 3</li>
+                            </ul>
+                        </div>
+                    </li>
+                ))}
             </ul>
 
         </nav>

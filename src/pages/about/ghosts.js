cd74443476d168cb061d81b651a342de921828da
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-03-13 13:37:18 -0700

    started styling ghosts page, made nav sticky and fixed footer styling

--- src/pages/about/ghosts.js
+++ src/pages/about/ghosts.js
@@ -1,7 +1,30 @@
 import React from "react";
 const AboutGhosts = () => {
     return (
-        <div>ghosts</div>
+        <div class="pageOn">
+            <h1>Ghosts</h1>
+            <div class="cardpgblurb">meet our ghosts!</div>
+            <div class="card card1">
+                <img src="" alt="photo of Mary" class="card1pic cardoddpic"/>
+                <div class="card1title cardoddtitle">Mary</div>
+                <div class="card1blurb cardoddblurb">about worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
+                slkfjslkdj theiaohdlk thekla about worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
+                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
+                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
+                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
+                slkfjslkdj theiaohdlk thekla</div>
+            </div>
+            <div class="card card2">
+                <img src="" alt="photo of Mae" class="card2pic cardevenpic"/>
+                <div class="card2title cardeventitle">Mae</div>
+                <div class="card2blurb cardevenblurb">about worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
+                slkfjslkdj theiaohdlk thekla about worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
+                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
+                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
+                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
+                slkfjslkdj theiaohdlk thekla</div>
+            </div>
+            </div>
     )
 }
 export default AboutGhosts;

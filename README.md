235e9e04c30198cfb1cc4f110d630a7640e6befa
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-03-14 12:10:49 -0700

    added homepage content, nav background and made page font sans serif

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 css dropdown menu tutorial https://www.w3schools.com/howto/howto_css_dropdown.asp
 hero image public domain site https://unsplash.com/photos/LNcoblo8nj8
+photo gallery code https://codepen.io/yoksel/pen/PooVvwZ
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,7 +1,27 @@
 import React from "react";
 const Home = () => {
     return (
-        <div>homepage</div>
+        <div class="pageOn homepg">
+            <div class="welcome home">
+                <h1 class="welcomeh line1">Welcome</h1>
+                <h1 class="welcomeh line2">to the<span class="bnbname"> Whimsy Bed and Breakfast</span></h1>
+                <div class="welcomeblurb">We'd love to have you! A small family run establishment we offer a quiet reprieve from the bustle of the city with a touch of Whimsy. Passed down through generations our three story colonial mansion has been offers  all the charm and history of the past with the modern amenities that make life comfortable. The sprawling grounds and gardeners are perfect for peacefull walks, artistic inspiration and romantic reprieves. We have experience hosting weddings, writer's retreats and even the odd seance! </div>
+                <div class="wlinks">for more info on ghost tours click here</div>
+                <div class="wlinks">to contact us click here</div>
+            </div>
+            <div class="home gallery wrapper">
+                <img src="https://source.unsplash.com/random/600x600?water" alt="" />
+                <img src="https://source.unsplash.com/random/600x600?summer" alt="" />
+                <img src="https://source.unsplash.com/random/600x600?plants" alt="" />
+                <img src="https://source.unsplash.com/random/600x600?snow" alt="" />
+                <img src="https://source.unsplash.com/random/600x600?roses" alt="" />
+                <img src="https://source.unsplash.com/random/600x600?sky" alt="" />
+                <img src="https://source.unsplash.com/random/600x600?nature" alt="" />
+                <img src="https://source.unsplash.com/random/600x600?blossom" alt="" />
+                <img src="https://source.unsplash.com/random/600x600?ice" alt="" />
+                <img src="https://source.unsplash.com/random/600x600?spring" alt="" />
+            </div>
+        </div>
     )
 }
 

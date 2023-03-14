dde94deee029f9ad1a24b3b334f3110f55ad7f7c
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-03-14 15:14:09 -0700

    created basic card page template

--- src/pages/about/ghosts.js
+++ src/pages/about/ghosts.js
@@ -1,38 +1,67 @@
 import React from "react";
+import place from '../../assets/place.png';
 const AboutGhosts = () => {
     return (
         <div class="pageOn cardpg">
+            <div class="cardpgtitle">
             <h1>Ghosts</h1>
             <div class="cardpgblurb">meet our ghosts!</div>
-            <div class="card card1">
-                <img src="" alt="photo of Mary" class="card1pic cardoddpic"/>
-                <div class="cardoddtitle">Mary</div>
-                <div class="cardoddblurb">about worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk thekla about worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk thekla</div>
             </div>
-            <div class="card card2">
-                <img src="" alt="photo of Mae" class="card2pic cardevenpic"/>
-                <div class="card2title cardeventitle">Mae</div>
-                <div class="card2blurb cardevenblurb">about worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk thekla about worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk thekla</div>
+            <div class="card odd">
+                <div class="cardimg"><img class="cardpic" src="" alt=""/></div>
+                <div class="cardbody">
+                    <h2>Jane</h2>
+                    <p>about</p>
+                </div>
             </div>
-            <div class="card card3">
-                <img src="" alt="photo of Mary" class="card3pic cardoddpic"/>
-                <div class="cardoddtitle">Mary</div>
-                <div class="cardoddblurb">about worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk thekla about worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk theklaabout worud wklnafiu asdlkfja;w akdfj;alk awekjf ijsd' adlkfjoiw adkjflskjapc ajsldkfja
-                slkfjslkdj theiaohdlk thekla</div>
+            <div class="card even">
+                
+                <div class="cardbody">
+                    <h2>Mary</h2>
+                    <p>about
+                    about The city rang with the hum and buzz of activity that marked all cities, albeit
+this one carried a distinctly medieval feel. Nyx trod the path of rough,
+uneven stone that ran between crude structures, most of them cobbled
+together out of mud and vegetation that had been pressed and dried to form
+a sheet-like material.
+The nicer structures were made of the same onyx stone as the wall, but
+every building without fail shared the same roof, made of a thatched, black
+straw-like material resting on a latticework built from white poles. Nyx
+edged closer to the buildings, wondering where the white poles
+                    </p>
+                </div>
+                <div class="cardimg"><img class="cardpic" src="" alt=""/></div>
+            </div>
+            <div class="card odd">
+                <div class="cardimg"><img class="cardpic" src="" alt=""/></div>
+                <div class="cardbody">
+                    <h2>Jane</h2>
+                    <p>about The city rang with the hum and buzz of activity that marked all cities, albeit
+this one carried a distinctly medieval feel. Nyx trod the path of rough,
+uneven stone that ran between crude structures, most of them cobbled
+together out of mud and vegetation that had been pressed and dried to form
+a sheet-like material.
+The nicer structures were made of the same onyx stone as the wall, but
+every building without fail shared the same roof, made of a thatched, black
+straw-like material resting on a latticework built from white poles. Nyx
+edged closer to the buildings, wondering where the white poles</p>
+                </div>
+            </div>
+            <div class="card even">
+               
+                <div class="cardbody">
+                    <h2>Mary</h2>
+                    <p>aboutabout
+                    about The city rang with the hum and buzz of activity that marked all cities, albeit
+this one carried a distinctly medieval feel. Nyx trod the path of rough,
+uneven stone that ran between crude structures, most of them cobbled
+together out of mud and vegetation that had been pressed and dried to form
+a sheet-like material.
+The nicer structures were made of the same onyx stone as the wall, but
+every building without fail shared the same roof, made of a thatched, black
+straw-like material resting on a latticework built from white </p>
+                </div>
+                <div class="cardimg"><img class="cardpic" src="" alt=""/></div>
             </div>
             </div>
     )

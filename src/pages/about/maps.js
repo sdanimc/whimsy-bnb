4704306e9154ac87642302c2e32ea3b90b3cebe4
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-03-15 10:52:05 -0700

    base page layout for hiking and maps pages

--- src/pages/about/maps.js
+++ src/pages/about/maps.js
@@ -1,7 +1,22 @@
 import React from "react";
 const AboutMaps = () => {
     return (
-        <div>Maps</div>
+        <div class="mapspg">
+            <div class="mapstitle">
+                <h1>Maps</h1>
+                <div class="mapsblurb">Find your way around! Maps are available for free at the front desk</div>
+            </div>
+            <div class="mapsbody">
+                <div class="innmap">
+                    <img src="" alt="" />
+                    <p>Whimsy Inn and Grounds Map</p>
+                </div>
+                <div class="downtownmap">
+                    <img src="" alt="" />
+                    <p>Map of Downtown Larksville</p>
+                </div>
+            </div>
+        </div>
     )
 }
 
--- src/pages/activities/hiking.js
+++ src/pages/activities/hiking.js
@@ -1,7 +1,19 @@
 import React from "react";
 const Hiking = () => {
     return (
-        <div>Local Hikes</div>
+        <div class="mapspg">
+        <div class="mapstitle">
+            <h1>Local Hiking</h1>
+            <div class="mapsblurb">We have great hiking here! Maps are available at the front desk or the National Park Service</div>
+        </div>
+        <div class="mapsbody">
+            <div class="hikemap">
+                <img src="" alt="" />
+                <p>Some of are local hikes</p>
+                <p>Check out the National Parks Services Website for more information or ask at the front desk!</p>
+            </div>
+        </div>
+    </div>
     )
 }
 export default Hiking;

6b08abbd596588769d36d650592851327b2be57f
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-03-15 11:13:03 -0700

    more to do base page design completed

--- src/pages/activities/moreinfo.js
+++ src/pages/activities/moreinfo.js
@@ -1,7 +1,14 @@
 import React from "react";
 const MoreInfo = () => {
     return (
-        <div>Events and More</div>
+        <div class="moretodopg">
+            <h1>More to See and Do!</h1>
+            <p>In addition to all the year round fun, we frequently host one in a lifetime experiences, seasonal gatherings and boutique events.
+                From RenFairs and crafter's markets to DnD nights, Mystery Theatre Dinners and Book clubs, there's lots to see and entertain.
+                To see what events we're hosting during your stay, check out our calendar. For help planning your trip to make the most of your
+                stay, or to plan an event of your own, contact us! We'd love to help
+            </p>
+        </div>
     )
 }
 

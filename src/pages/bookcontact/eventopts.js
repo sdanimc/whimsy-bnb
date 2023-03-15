75ca1e1a2b88f526f8078208b0c62851a2765d85
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-03-15 09:21:05 -0700

    copied in card page template to all pages that will use it, titles were edited to confirm page works but content editing is necessary

--- src/pages/bookcontact/eventopts.js
+++ src/pages/bookcontact/eventopts.js
@@ -1,7 +1,68 @@
 import React from "react";
 const Events = () => {
     return (
-        <div>Events</div>
+        <div class="pageOn cardpg">
+            <div class="cardpgtitle">
+            <h1>Events</h1>
+            <div class="cardpgblurb">Weddings, Reunions, Retreats and More!</div>
+            </div>
+            <div class="card odd">
+                <div class="cardimg"><img class="cardpic" src="" alt=""/></div>
+                <div class="cardbody">
+                    <h2>Jane</h2>
+                    <p>about</p>
+                </div>
+            </div>
+            <div class="card even">
+                
+                <div class="cardbody">
+                    <h2>Mary</h2>
+                    <p>about
+                    about The city rang with the hum and buzz of activity that marked all cities, albeit
+this one carried a distinctly medieval feel. Nyx trod the path of rough,
+uneven stone that ran between crude structures, most of them cobbled
+together out of mud and vegetation that had been pressed and dried to form
+a sheet-like material.
+The nicer structures were made of the same onyx stone as the wall, but
+every building without fail shared the same roof, made of a thatched, black
+straw-like material resting on a latticework built from white poles. Nyx
+edged closer to the buildings, wondering where the white poles
+                    </p>
+                </div>
+                <div class="cardimg"><img class="cardpic" src="" alt=""/></div>
+            </div>
+            <div class="card odd">
+                <div class="cardimg"><img class="cardpic" src="" alt=""/></div>
+                <div class="cardbody">
+                    <h2>Jane</h2>
+                    <p>about The city rang with the hum and buzz of activity that marked all cities, albeit
+this one carried a distinctly medieval feel. Nyx trod the path of rough,
+uneven stone that ran between crude structures, most of them cobbled
+together out of mud and vegetation that had been pressed and dried to form
+a sheet-like material.
+The nicer structures were made of the same onyx stone as the wall, but
+every building without fail shared the same roof, made of a thatched, black
+straw-like material resting on a latticework built from white poles. Nyx
+edged closer to the buildings, wondering where the white poles</p>
+                </div>
+            </div>
+            <div class="card even">
+               
+                <div class="cardbody">
+                    <h2>Mary</h2>
+                    <p>aboutabout
+                    about The city rang with the hum and buzz of activity that marked all cities, albeit
+this one carried a distinctly medieval feel. Nyx trod the path of rough,
+uneven stone that ran between crude structures, most of them cobbled
+together out of mud and vegetation that had been pressed and dried to form
+a sheet-like material.
+The nicer structures were made of the same onyx stone as the wall, but
+every building without fail shared the same roof, made of a thatched, black
+straw-like material resting on a latticework built from white </p>
+                </div>
+                <div class="cardimg"><img class="cardpic" src="" alt=""/></div>
+            </div>
+            </div>
     )
 }
 export default Events;

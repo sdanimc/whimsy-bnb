930f5a0c07bab0b1096b2116c7ab42c89fcd97b3
sdanimc <116037382+sdanimc@users.noreply.github.com>
2023-03-15 10:17:21 -0700

    created basic history page

--- src/pages/about/history.js
+++ src/pages/about/history.js
@@ -1,7 +1,32 @@
 import React from "react";
 const AboutHistory = () => {
     return (
-        <div>history</div>
+        <div class="historypg pageOn">
+            <div class="historytitle"><h1>Our Story</h1></div>
+            <div class="historybody">
+                <div class="historyblock1">
+                    <div class="historytext1">blurb about the history of the house who built it when where stayed in family reputation as
+                        haunted etcblurb about the history of the house who built it when where stayed in family reputation as
+                        haunted etcblurb about the history of the house who built it when where stayed in family reputation as
+                        haunted etcblurb about the history of the house who built it when where stayed in family reputation as
+                        haunted etc</div>
+                    <div class="historypic1"><img src="" alt="photo" /></div>
+                </div>
+                <div class="historyblock2">
+                    <div class="historypic2"><img src="" alt="photo" /></div>
+                    <div class="historytext2">blurb about restoration current owners when they got the house their connection to it etcblurb about the history of the house who built it when where stayed in family reputation as
+                        haunted etcblurb about the history of the house who built it when where stayed in family reputation as
+                        haunted etcblurb about the history of the house who built it when where stayed in family reputation as
+                        haunted etcblurb about the history of the house who built it when where stayed in family reputation as
+                        haunted etc</div>
+                </div>
+                <div class="historyblock3">
+                    <div class="historytext3"> For more infomation about our ghosts click here or join us for our ghost tours listed here!</div>
+                </div>
+            </div>
+
+
+        </div>
     )
 }
 export default AboutHistory;
